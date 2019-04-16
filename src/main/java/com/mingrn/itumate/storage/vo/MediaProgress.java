e0550c273ff34870e2078c4f784d5f534eb8c360
MinGRn <MinGRn97@gmail.com>
2019-04-16 22:22:26 +0800

    未做功能操作;

--- src/main/java/com/mingrn/itumate/storage/vo/MediaProgress.java
+++ src/main/java/com/mingrn/itumate/storage/vo/MediaProgress.java
@@ -2,6 +2,8 @@ package com.mingrn.itumate.storage.vo;
 
 import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
+
 /**
  * 媒体进度条
  *
@@ -9,7 +11,9 @@ import org.springframework.stereotype.Component;
  * @date 2019/4/16 19:57
  */
 @Component
-public class MediaProgress {
+public class MediaProgress implements Serializable {
+
+    public static final Long serialVersionUID = -1L;
 
     /**
      * 已加载数据
